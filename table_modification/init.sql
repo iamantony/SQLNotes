ad56b2f7394d5d6b7a8ac17d6a255dab98fe99eb
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-09 15:25:34 +0300

    SQL Table modifications

---
+++ table_modification/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE movie (
+  name TEXT,
+  year INTEGER
+);
---
+++ table_modification/table_modification.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO movie(name, year) VALUES ('My movie', 1995);
+INSERT INTO movie(name, year) VALUES ('Old movie', 1900);
+
+ALTER TABLE movie ADD length INTEGER;
+ALTER TABLE movie ADD rating REAL DEFAULT 0.0;
+ALTER TABLE movie ADD description TEXT DEFAULT 'No description';
+ALTER TABLE movie ADD id SERIAL PRIMARY KEY;
+
+ALTER TABLE movie DROP rating;
