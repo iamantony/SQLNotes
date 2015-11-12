4374f365d60faaa79eda50b2ed29de9677ca5b38
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-12 16:31:49 +0300

    UPDATE statement

---
+++ update/update.sql
@@ -0,0 +1,5 @@
+﻿-- Update all rows in a table
+UPDATE movie SET year = 1900;
+-- Update special rows in the table
+UPDATE movie SET year = 2015 WHERE name = 'Lobster';
+UPDATE movie SET year = 2002 WHERE name = 'The Pianist' AND year = 1900;
