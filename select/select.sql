60ffb5518ea32af9e1bb1594f8b62f07c473dc7f
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-13 12:21:35 +0300

    Add SELECT

---
+++ select/select.sql
@@ -0,0 +1,9 @@
+﻿SELECT * FROM item;
+SELECT cost_price FROM item;
+
+SELECT * FROM item 
+  WHERE cost_price > (SELECT avg(cost_price) FROM item) AND
+  cost_price < (SELECT avg(sell_price) FROM item);
+
+SELECT * FROM stock WHERE item_id IN
+  (SELECT item_id FROM item WHERE cost_price > 10);
