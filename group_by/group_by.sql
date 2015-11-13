8eaf53f20278ff8c3ac23137999aaaa63458fc90
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-13 11:33:25 +0300

    Add GROUP BY

---
+++ group_by/group_by.sql
@@ -0,0 +1,3 @@
+﻿SELECT count(*), town FROM customer GROUP BY town ORDER BY town;
+SELECT count(*), town FROM customer GROUP BY town HAVING count(*) > 1 ORDER BY town;
+SELECT count(*), town FROM customer WHERE town <> 'Bingham' GROUP BY town HAVING count(*) > 1 ORDER BY town;
