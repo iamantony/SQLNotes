3b5a81a0d2110018498b30b2ef6536fb3c87381c
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-13 11:33:07 +0300

    Add aggregates

---
+++ aggregates/aggregates.sql
@@ -0,0 +1,29 @@
+﻿-- count()
+SELECT count(*) FROM customer;
+SELECT count(*) FROM customer WHERE lname = 'Matthew';
+
+-- count() with GROUP BY
+SELECT count(*), town FROM customer GROUP BY town ORDER BY town;
+
+-- count(column_name)
+SELECT count(*) FROM customer;
+-- will not count NULL values
+SELECT count(phone) FROM customer;
+
+SELECT count(DISTINCT title) FROM customer;
+
+-- min()
+SELECT min(customer_id) FROM customer;
+SELECT min(fname) FROM customer;
+SELECT min(phone) FROM customer;
+
+-- max()
+SELECT max(customer_id) FROM customer;
+SELECT max(fname) FROM customer;
+SELECT max(phone) FROM customer;
+
+-- sum()
+SELECT sum(customer_id) FROM customer;
+
+-- avg()
+SELECT avg(customer_id) FROM customer;
