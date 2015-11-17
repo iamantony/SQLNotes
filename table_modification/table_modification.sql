171e2e73d98f09c31670d89a26bdfe423272f2db
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-17 12:58:24 +0300

    Add table modifications - rename, constraints, checks

--- table_modification/table_modification.sql
+++ table_modification/table_modification.sql
@@ -7,3 +7,8 @@ ALTER TABLE movie ADD description TEXT DEFAULT 'No description';
 ALTER TABLE movie ADD id SERIAL PRIMARY KEY;
 
 ALTER TABLE movie DROP rating;
+ALTER TABLE movie RENAME COLUMN description TO descr;
+ALTER TABLE movie ADD CONSTRAINT lenconstr UNIQUE(length);
+ALTER TABLE movie DROP CONSTRAINT lenconstr;
+ALTER TABLE movie ALTER COLUMN length SET DEFAULT 0;
+ALTER TABLE movie ADD CHECK(descr <> '');
