60dc991dd5411da5403ffb962beaca00869df90a
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-17 14:43:46 +0300

    Add functions

---
+++ function/function.sql
@@ -0,0 +1,41 @@
+﻿-- Basic
+CREATE FUNCTION add_one(INTEGER) RETURNS INTEGER AS '
+  BEGIN
+    RETURN $1 + 1;
+  END;
+'  LANGUAGE 'plpgsql';
+
+SELECT * FROM add_one(1);
+
+-- Without quoting
+CREATE FUNCTION add_two(INTEGER) RETURNS INTEGER AS
+$BODY$
+  BEGIN
+    RETURN $1 + 2;
+  END;
+$BODY$
+  LANGUAGE 'plpgsql';
+
+SELECT * FROM add_two(1);
+
+-- With name for argument
+CREATE FUNCTION add_three(value INTEGER) RETURNS INTEGER AS
+$BODY$
+  BEGIN
+    RETURN value + 3;
+  END;
+$BODY$
+  LANGUAGE 'plpgsql';
+
+SELECT * FROM add_three(1);
+
+-- Function overloading
+CREATE FUNCTION add_three(value REAL) RETURNS INTEGER AS
+$BODY$
+  BEGIN
+    RETURN value;
+  END;
+$BODY$
+  LANGUAGE 'plpgsql';
+
+SELECT * FROM add_three(1.0);
