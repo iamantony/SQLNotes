ce68953223f51c226907c0a5ce66fe81277df9bc
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-19 12:54:37 +0300

    Add TRIGGER

---
+++ triggers/cleanup.sql
@@ -0,0 +1,4 @@
+﻿DROP TRIGGER stock_upd ON stock;
+DROP FUNCTION func();
+
+DROP TABLE customer, item, orderinfo, stock, test_stock;
---
+++ triggers/triggers.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION func() returns TRIGGER AS $$
+DECLARE
+  val INTEGER;
+BEGIN
+  val := tg_argv[0];
+  val := new.quantity + val;
+
+  INSERT INTO test_stock(item_id, quantity) VALUES(new.item_id, val);
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER stock_upd BEFORE INSERT OR UPDATE ON stock
+  FOR EACH ROW EXECUTE PROCEDURE func(120);
+
+INSERT INTO stock(item_id, quantity) VALUES(11,2);
+UPDATE stock SET quantity = 15 WHERE item_id = 2;
